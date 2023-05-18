d1963c6c5f3bc9d9b6e772816933aed26ed2fbb1
alvo254 <alvinkhamron@gmail.com>
2023-05-18 13:54:28 +0300

    DB connection

--- database/databaseCon.go
+++ database/databaseCon.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"github.com/joho/godotenv"
@@ -12,5 +13,23 @@ import (
 )
 
 func DBInstance() *mongo.Client{
+	error := godotenv.Load(".env")
+	if error != nil{
+		log.Fatal("Error loading .env files")
+	}
 
+	MongoDB := os.Getenv("MONGO_URL")
+
+	client, error := mongo.NewClient(options.Client().ApplyURI(MongoDB))
+	if error != nil{
+		log.Fatal(error)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
+	defer cancel()
+	error := client.Connect(ctx)
+	if error != nil{
+		log.Fatal(error)
+	}
+	fmt.Println("Connected to mongDB")
 }
