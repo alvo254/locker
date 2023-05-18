ade25dca9b1c11e97eb627559587025dfda288ba
alvo254 <alvinkhamron@gmail.com>
2023-05-18 13:34:33 +0300

    rm cached

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,2 +1,6 @@
 package controllers
 
+import (
+
+)
+
---
+++ database/databaseCon.go
@@ -0,0 +1,16 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go/mongodb.org/mongo-driver/mongo/options"
+	"os"
+	"time"
+	
+)
+
+func DBInstance() *mongo.Client{
+
+}
