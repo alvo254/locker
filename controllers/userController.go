e200def3f8d780be4c53ce973fd25e16573416ea
alvo254 <alvinkhamron@gmail.com>
2023-05-18 14:15:50 +0300

    user controller

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,6 +1,57 @@
 package controllers
 
 import (
+	"go.mongodb.org/mongo-driver/mongo"
+	"time"
+	"github.com/gin-gonic/gin"
+	"log"
+	"fmt"
+	"strconv"
+	"context"
+	"net/http"
+	"github.com/go-playground/validator/v10"
+	helper "github.com/alvo254/locker/helpers"
+	"github.com/alvo254/locker/models"
+	"github.com/alvo254/locker/helpers"
+	"golang.org/x/crypto/bcrypt"
 
 )
+
+var userCollection *mongo.Collection = database.OpenCollection(database.client, "user")
+var validate = validator.New()
+
+func HashPassword(){
+
+}
+
+func VerifyPassword(){
+
+}
+
+func SignUp(){
+
+}
+
+func Login(){
+
+}
+
+func GetUsers(){
+
+}
+
+//Only admin can acess
+func GetUser() gin.HandlerFunc{
+	return func(c *gin.Context){
+		//user_id comes from routes (specified :user_id)
+		userId := c.Param("user_id")
+		helper.MatchUserTypeToUid(c, userId); error != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": error.Error()})
+		}
+	}
+
+}
+
+
+
 
