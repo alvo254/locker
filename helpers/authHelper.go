cd01ba9982264e556da7eac52b9d9d2fe749f2dc
alvo254 <alvinkhamron@gmail.com>
2023-05-18 14:24:29 +0300

    auth helper

--- helpers/authHelper.go
+++ helpers/authHelper.go
@@ -1,2 +1,32 @@
 package helpers
 
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+
+)
+
+func CheckUserType(c *gin.Context, role string){
+	userType := c.GetString("user_type")
+	err = nil
+	if userType != role{
+		err = errors.New("Unauthorized to acces this resources")
+		return err
+	}
+	return err
+}
+
+
+func MatchUserTypeToUid(c *gin.Context, userId string) (err error){
+	userType := c.GetString("user_type")
+	uid := c.GetString("uid")
+
+	err = nil
+
+	if userType == "USER" && uid != userId{
+		err = errors.New("Unauthorized to acces this resources")
+		return err
+	}
+	err = CheckUserType(c, userType)
+	return err
+}
